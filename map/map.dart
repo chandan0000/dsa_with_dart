627d77067cb8f72f051da4fda4ece58c0124a871
chandan kumar <you@example.com>
2022-02-28 23:11:02 +0530

    -

---
+++ map/map.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Creating Map
+  var gfg = {'position1': 'Geeks' 'for' 'Geeks'};
+
+  // Printing Its content before insertion
+  print(gfg);
+
+  // Inserting a new value in Map
+  gfg['position0'] = 'Welcome to ';
+
+  // Printing Its content after insertion
+  print(gfg);
+
+  // Printing Specific Content
+  // Keys is defined
+}
